a114b18e53c65519980a99673634bd2513e85c7b
soomin <soomin.song@kt.com>
2021-03-22 17:42:20 +0900

    config file modify

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,19 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
+import modules from './modules';
+import createLogger from 'vuex/dist/logger';
+
+const debug = process.env.NODE_ENV !== 'production';
 
 Vue.use(Vuex);
 
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules,
+  strict: debug,
+  plugins: debug ? [createLogger(), vuexLocal.plugin] : [vuexLocal.plugin],
 });
---
+++ src/store/modules/data.store.ts
@@ -0,0 +1,23 @@
+const state = {
+  test: false,
+};
+
+const getters = {
+  TEST: state => state.test,
+};
+
+const mutations = {
+  SET_TEST: state => {
+    state.test = !state.test;
+  },
+};
+
+const actions = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+};
